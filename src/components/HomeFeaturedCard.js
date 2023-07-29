d6ceb19669662f0ccaaeecc7bd9afd697b35c1eb
Moniruzzaman Sujon <git.sujon@gmail.com>
2023-07-29 20:59:43 +0600

    Featured product showcasing complete

---
+++ src/components/HomeFeaturedCard.js
@@ -0,0 +1,72 @@
+import Image from "next/image";
+import React from "react";
+import RatingStar from "./UI/RatingStar";
+import Link from "next/link";
+
+const HomeFeaturedCard = ({ products }) => {
+  // const {
+  //     _id,
+  //     product_name,
+  //     image,
+  //     category,
+  //     status,
+  //     price,
+  //     description,
+  //     key_features,
+  //     individual_rating,
+  //     average_rating,
+  //     reviews,
+  //     is_home_featured,
+  //   } = product;
+
+  return (
+    <div>
+        <h2 className="text-2xl font-bold text-slate-700 ">Featured products</h2>
+        <div className="divider"></div> 
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 ">
+        {products.map((product) => {
+          return (
+            <Link href={`products/item/${product?._id}`} 
+              key={product?._id}
+              className="  transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg"
+            >
+              <Image
+                className="h-48 w-full object-cover object-center"
+                src={
+                    "https://in-media.apjonlinecdn.com/magefan_blog/What_is_FPS_and_How_Can_You_Optimise_Your_PC_for_Gaming_1.png"
+                  }
+                alt="dd"
+                width={400}
+                height={192}
+              />
+
+              <div className="p-4">
+                <h2 className="mb-2 text-lg font-medium dark:text-white text-gray-900">
+                  {product?.product_name}
+                </h2>
+                <p className="mb-2 text-base dark:text-gray-300 text-gray-700">
+                  {product?.category}
+                </p>
+                <div className="flex items-center">
+                  <p className="mr-2 text-lg font-semibold text-gray-900 dark:text-white">
+                    ${product?.price}
+                  </p>
+                  <p className="text-base  font-medium text-gray-500  dark:text-gray-300">
+                    {product?.status}
+                  </p>
+                  <div className="ml-auto text-base font-medium text-green-500 flex items-center gap-2">
+                
+                    <input type="radio" name="rating-1" className="mask mask-star" />/
+                    <p>{product?.average_rating}</p>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default HomeFeaturedCard;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
+import HomeFeaturedCard from "@/components/HomeFeaturedCard";
 import HomeLayout from "@/components/Layout/HomeLayout";
 import Head from "next/head";
 
-const HomePage = () => {
+const HomePage = ({products}) => {
+
+
+
   return (
     <>
       <Head>
@@ -9,7 +13,7 @@ const HomePage = () => {
         <meta property="og:title" content="At PCWizardry, we are passionate about empowering tech enthusiasts and professionals to unleash the full potential of their computing dreams. Our mission is to provide a seamless and immersive PC building experience, where creativity meets cutting-edge technology." key="title" />
       </Head>
       <div>
-       
+       <HomeFeaturedCard products={products}/>
       </div>
     </>
   );
@@ -20,3 +24,20 @@ export default HomePage;
 HomePage.getLayout = function getLayout(page) {
   return <HomeLayout>{page}</HomeLayout>;
 };
+
+
+
+
+
+export async function getStaticProps() {
+
+
+  const res = await fetch(`http://localhost:3000/api/products/home_featured`);
+  const data = await res.json();
+
+  return {
+    props: {
+      products: data?.data,
+    },
+  };
+}
